89244dbe953f9be6a75dd1928eb9ab1b1202ee80
Shubham Mishra <129315610+Animetion@users.noreply.github.com>
2023-04-02 23:39:44 +0530

    header

--- src/component/layout/Header/Header.js
+++ src/component/layout/Header/Header.js
@@ -1,43 +1,84 @@
-import React from 'react';
-import { ReactNavbar } from "overlay-navbar";
+// import React from 'react';
+// import { ReactNavbar } from "overlay-navbar";
 import logo from "../../../images/logo.png";
 
-const options = {
-  burgerColor: "#eb4034",
-  burgerColorHover: "#eb4034",
-  logo,
-  logoWidth: "20vmax",
-  navColor1: "white",
-  logoHoverSize: "10px",
-  logoHoverColor: "#eb4034",
-  link1Text: "Home",
-  link2Text: "Products",
-  link3Text: "Contact",
-  link4Text: "About",
-  link1Url: "/",
-  link2Url: "/products",
-  link3Url: "/contact",
-  link4Url: "/about",
-  link1Size: "1.3vmax",
-  link1Color: "rgba(35, 35, 35,0.8)",
-  nav1justifyContent: "flex-end",
-  nav2justifyContent: "flex-end",
-  nav3justifyContent: "flex-start",
-  nav4justifyContent: "flex-start",
-  link1ColorHover: "#eb4034",
-  link1Margin: "1vmax",
-  profileIconUrl: "/login",
-  profileIconColor: "rgba(35, 35, 35,0.8)",
-  searchIconColor: "rgba(35, 35, 35,0.8)",
-  cartIconColor: "rgba(35, 35, 35,0.8)",
-  profileIconColorHover: "#eb4034",
-  searchIconColorHover: "#eb4034",
-  cartIconColorHover: "#eb4034",
-  cartIconMargin: "1vmax",
-};
+// const options = {
+//   burgerColor: "#eb4034",
+//   burgerColorHover: "#eb4034",
+//   logo,
+//   logoWidth: "20vmax",
+//   navColor1: "white",
+//   logoHoverSize: "10px",
+//   logoHoverColor: "#eb4034",
+//   link1Text: "Home",
+//   link2Text: "Products",
+//   link3Text: "Contact",
+//   link4Text: "About",
+//   link1Url: "/",
+//   link2Url: "/products",
+//   link3Url: "/contact",
+//   link4Url: "/about",
+//   link1Size: "1.3vmax",
+//   link1Color: "rgba(35, 35, 35,0.8)",
+//   nav1justifyContent: "flex-end",
+//   nav2justifyContent: "flex-end",
+//   nav3justifyContent: "flex-start",
+//   nav4justifyContent: "flex-start",
+//   link1ColorHover: "#eb4034",
+//   link1Margin: "1vmax",
+//   profileIconUrl: "/login",
+//   profileIconColor: "rgba(35, 35, 35,0.8)",
+//   searchIconColor: "rgba(35, 35, 35,0.8)",
+//   cartIconColor: "rgba(35, 35, 35,0.8)",
+//   profileIconColorHover: "#eb4034",
+//   searchIconColorHover: "#eb4034",
+//   cartIconColorHover: "#eb4034",
+//   cartIconMargin: "1vmax",
+// };
 
-const Header = () => {
-  return <ReactNavbar {...options} />;
-};
+// const Header = () => {
+//   return <ReactNavbar {...options} />;
+// };
+
+// export default Header;
+
+
+
+import { useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import "./Header.css";
+
+function Header() {
+	const navRef = useRef();
+
+	const showNavbar = () => {
+		navRef.current.classList.toggle(
+			"responsive_nav"
+		);
+	};
+
+	return (
+		<header>
+			<a href=""><img src={logo} alt="" /></a>
+			<nav ref={navRef}>
+				<a href="/#">Home</a>
+				<a href="/#">My work</a>
+				<a href="/#">Blog</a>
+				<a href="/#">About me</a>
+				<button
+					className="nav-btn nav-close-btn"
+					onClick={showNavbar}>
+					<FaTimes />
+				</button>
+			</nav>
+			<button
+				className="nav-btn"
+				onClick={showNavbar}>
+				<FaBars />
+			</button>
+		</header>
+	);
+}
 
 export default Header;
+              
