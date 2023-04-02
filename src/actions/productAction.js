b21a2282ade544debb13a1f7294bd151ea06ac86
Shubham Mishra <129315610+Animetion@users.noreply.github.com>
2023-04-02 19:32:16 +0530

    Update productAction.js

--- src/actions/productAction.js
+++ src/actions/productAction.js
@@ -39,10 +39,10 @@ export const getProduct =
       dispatch({ type: ALL_PRODUCT_REQUEST });
 
       // let link = `/api/v1/products`;
-      let link = `https://animetion.onrender.com//api/v1/products?keyword=${keyword}&page=${currentPage}&price[gte]=${price[0]}&price[lte]=${price[1]}&ratings[gte]=${ratings}`;
+      let link = `https://animetion.onrender.com/api/v1/products?keyword=${keyword}&page=${currentPage}&price[gte]=${price[0]}&price[lte]=${price[1]}&ratings[gte]=${ratings}`;
 
       if (category) {
-        link = `https://animetion.onrender.com//api/v1/products?keyword=${keyword}&page=${currentPage}&price[gte]=${price[0]}&price[lte]=${price[1]}&category=${category}&ratings[gte]=${ratings}`;
+        link = `https://animetion.onrender.com/api/v1/products?keyword=${keyword}&page=${currentPage}&price[gte]=${price[0]}&price[lte]=${price[1]}&category=${category}&ratings[gte]=${ratings}`;
       }
 
       const { data } = await axios.get(link);
